3372db1e57faf05e5204905cbfda6f0e20675551
juliocmorales <40004698+juliocmorales@users.noreply.github.com>
2018-07-06 11:38:45 -0500

    primer commit

---
+++ netflix-zuul-proxy/src/main/java/com/neoris/microservicios/NetflixZuulProxyApplication.java
@@ -0,0 +1,14 @@
+package com.neoris.microservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class NetflixZuulProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulProxyApplication.class, args);
+	}
+}
